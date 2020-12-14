ff104e61a6a2909ff39cb0c0453a479c18830403
LinJianLin <2993354@qq.com>
2020-12-14 17:53:58 +0800

    修复负数生命值下血条显示问题

--- src/components/index.vue
+++ src/components/index.vue
@@ -469,7 +469,7 @@
                 :title="item.name"
               >
                 ({{ item.prefix == 0 ? "无" : item.prefix }}){{ item.name }}
-                <Progress :percent="Math.max(item.life / item.maxLife * 100,0)">
+                <Progress :percent="Math.max(item.life,0) / item.maxLife * 100">
                   <span>{{ item.life }}</span>
                 </Progress>
                 <div slot="content">
